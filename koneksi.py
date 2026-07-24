6b8b99fcf79c403d430502d0c1d95f7163fa3484
adibaaul <adibaaul@gmail.com>
2026-07-24 10:20:41 +0700

    Refactor koneksi function for database connection
    Convert port to integer and fix ssl_disable parameter.

--- koneksi.py
+++ koneksi.py
@@ -5,10 +5,10 @@ def koneksi():
     db_config = st.secrets["database"]
     mydb = mysql.connector.connect(
         host=db_config["host"],
-        port=db_config["port"],
+        port=int(db_config["port"]),
         user=db_config["user"],
         password=db_config["password"],
         database=db_config["database"],
-        ssl_disable = False,
+        ssl_disabled = False,
     )
     return mydb
